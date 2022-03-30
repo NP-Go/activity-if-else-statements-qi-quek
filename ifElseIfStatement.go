8a8d0eedd2fbecea40bc29678abf401086b4ffeb
narwhal04 <q.quek7@gmail.com>
2022-03-30 23:54:08 +0800

    working code

--- ifElseIfStatement.go
+++ ifElseIfStatement.go
@@ -8,7 +8,15 @@ import (
 func evaluateValue(value int64) string {
 	replyMessage := ""
 	//insert your code here
-
+	if value%5 == 0 && value%6 == 0 {
+		replyMessage = "Value is divisible by both 5 and 6"
+	} else if value%5 == 0 && value%6 != 0 {
+		replyMessage = "value is divisible by 5 but not 6"
+	} else if value%6 == 0 && value%5 != 0 {
+		replyMessage = "value is divisible by 6 but not 5"
+	} else {
+		replyMessage = "Value is not divisable by both 5 or 6"
+	}
 	//Do not remove these lines
 	fmt.Println((replyMessage))
 	return replyMessage
